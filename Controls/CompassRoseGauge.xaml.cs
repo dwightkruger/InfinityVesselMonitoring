86acf1f8e02a0961263b0452dcfcd8650b191b16
Dwight Kruger <dwightkruger@mvinfinity.com>
2018-01-28 15:54:44 -0800

    Safe keeping

--- Controls/CompassRoseGauge.xaml.cs
+++ Controls/CompassRoseGauge.xaml.cs
@@ -40,11 +40,11 @@ namespace InfinityGroup.VesselMonitoring.Controls
             using (var textFormat = new CanvasTextFormat()
             {
                 HorizontalAlignment = CanvasHorizontalAlignment.Left,
-                VerticalAlignment = CanvasVerticalAlignment.Center,
+                VerticalAlignment = CanvasVerticalAlignment.Top,
                 FontSize = (float)this.TextFontSize,
             })
             {
-                Vector2 at = new Vector2(0, (float)sender.ActualHeight / 2);
+                Vector2 at = new Vector2(0, 0);
                 ds.DrawText(this.Text, at, this.GaugeColor, textFormat);
             }
 
@@ -67,16 +67,92 @@ namespace InfinityGroup.VesselMonitoring.Controls
             CanvasDrawingSession ds = args.DrawingSession;
 
             this.Center = new Vector2((float)sender.ActualWidth / 2, (float)sender.ActualHeight / 2);
+            float radius = (float)sender.ActualWidth / 2F - 2F;
+
+            // Draw the minor tics around the circumference
+            float minorTicLength = 15;
+            float minorTicstrokeWidth = 3;
+            for (int i = 0; i < 360; i += 10)
+            {
+                float angle = (float)(i + this.Value) % 360F;
+                float x1 = (float)Math.Cos(RadiansFromDegrees(angle)) * radius;
+                float y1 = (float)Math.Sin(RadiansFromDegrees(angle)) * radius;
+                float x2 = (float)Math.Cos(RadiansFromDegrees(angle)) * (radius - minorTicLength);
+                float y2 = (float)Math.Sin(RadiansFromDegrees(angle)) * (radius - minorTicLength);
+                using (CanvasPathBuilder cp = new CanvasPathBuilder(sender))
+                {
+                    cp.BeginFigure(new Vector2(x1, y1) + this.Center);
+                    cp.AddLine(new Vector2(x2, y2) + this.Center);
+                    cp.EndFigure(CanvasFigureLoop.Open);
+                    using (var geometry = CanvasGeometry.CreatePath(cp))
+                    {
+                        ds.DrawGeometry(geometry, this.GaugeColor, minorTicstrokeWidth);
+                    }
+                }
+            }
+
+            // Draw the major tics around the circumference
+            float majorTicLength = 30;
+            float majorTicstrokeWidth = 6;
+            for (int i=0; i<360; i+=30)
+            {
+                float angle = (float)(i+this.Value) % 360F;
+                float x1 = (float)Math.Cos(RadiansFromDegrees(angle)) * radius;
+                float y1 = (float)Math.Sin(RadiansFromDegrees(angle)) * radius;
+                float x2 = (float)Math.Cos(RadiansFromDegrees(angle)) * (radius - majorTicLength);
+                float y2 = (float)Math.Sin(RadiansFromDegrees(angle)) * (radius - majorTicLength);
+                using (CanvasPathBuilder cp = new CanvasPathBuilder(sender))
+                {
+                    cp.BeginFigure(new Vector2(x1,y1) + this.Center);
+                    cp.AddLine(new Vector2(x2, y2) + this.Center);
+                    cp.EndFigure(CanvasFigureLoop.Open);
+                    using (var geometry = CanvasGeometry.CreatePath(cp))
+                    {
+                        ds.DrawGeometry(geometry, this.GaugeColor, majorTicstrokeWidth);
+                    }
+                }
+
+                float atX = (float)Math.Cos(RadiansFromDegrees(angle)) * (radius - 1.8F*majorTicLength);
+                float atY = (float)Math.Sin(RadiansFromDegrees(angle)) * (radius - 1.8F*majorTicLength);
+                Vector2 at = new Vector2(atX, atY) + this.Center;
+
+                using (var textFormat = new CanvasTextFormat()
+                {
+                    HorizontalAlignment = CanvasHorizontalAlignment.Center,
+                    VerticalAlignment = CanvasVerticalAlignment.Center,
+                    FontSize = (float)this.LabelsFontSize * 1.5F,
+                })
+                {
+                    int label = Convert.ToInt32(i+90) % 360;
+                    if (000 == label)      ds.DrawText("N", at, this.GaugeColor, textFormat);
+                    else if (090 == label) ds.DrawText("E", at, this.GaugeColor, textFormat);
+                    else if (180 == label) ds.DrawText("S", at, this.GaugeColor, textFormat);
+                    else if (270 == label) ds.DrawText("W", at, this.GaugeColor, textFormat);
+                    else                   ds.DrawText(i.ToString(), at, this.GaugeColor, textFormat);
+                }
+            }
+
+            using (var textFormat = new CanvasTextFormat()
+            {
+                HorizontalAlignment = CanvasHorizontalAlignment.Right,
+                VerticalAlignment = CanvasVerticalAlignment.Bottom,
+                FontSize = (float)this.TextFontSize,
+            })
+            {
+                Vector2 at = new Vector2((float)sender.ActualWidth, (float)sender.ActualHeight*0.9F);
+                ds.DrawText("Mag", at, this.GaugeColor, textFormat);
+            }
 
             float length = this.Center.Y - this.Center.Y / 2F;
-            Vector2 start  = new Vector2(this.Center.X,          this.Center.Y - length);
-            Vector2 left   = new Vector2(this.Center.X - length, this.Center.Y);
-            Vector2 bottom = new Vector2(this.Center.X,          this.Center.Y + length);
-            Vector2 right = new Vector2(this.Center.X + length,  this.Center.Y);
-            Vector2 leftTop     = new Vector2((float)Math.Cos(RadiansFromDegrees(5*45)) * length/2F, (float)Math.Sin(RadiansFromDegrees(5*45)) * length/2F) + this.Center;
-            Vector2 leftBottom  = new Vector2((float)Math.Cos(RadiansFromDegrees(3*45)) * length / 2F, (float)Math.Sin(RadiansFromDegrees(3*45)) * length / 2F) + this.Center;
-            Vector2 rightTop    = new Vector2((float)Math.Cos(RadiansFromDegrees(7*45)) * length / 2F, (float)Math.Sin(RadiansFromDegrees(7*45)) * length / 2F) + this.Center;
-            Vector2 rightBottom = new Vector2((float)Math.Cos(RadiansFromDegrees(45)) * length / 2F, (float)Math.Sin(RadiansFromDegrees(45)) * length / 2F) + this.Center;
+
+            Vector2 start = new Vector2(0, 0 - length);
+            Vector2 left = new Vector2(0 - length, 0);
+            Vector2 bottom = new Vector2(0, 0 + length);
+            Vector2 right = new Vector2(0 + length, 0);
+            Vector2 leftTop = new Vector2((float)Math.Cos(RadiansFromDegrees(5 * 45)) * length / 2F, (float)Math.Sin(RadiansFromDegrees(5 * 45)) * length / 2F);
+            Vector2 leftBottom = new Vector2((float)Math.Cos(RadiansFromDegrees(3 * 45)) * length / 2F, (float)Math.Sin(RadiansFromDegrees(3 * 45)) * length / 2F);
+            Vector2 rightTop = new Vector2((float)Math.Cos(RadiansFromDegrees(7 * 45)) * length / 2F, (float)Math.Sin(RadiansFromDegrees(7 * 45)) * length / 2F);
+            Vector2 rightBottom = new Vector2((float)Math.Cos(RadiansFromDegrees(45)) * length / 2F, (float)Math.Sin(RadiansFromDegrees(45)) * length / 2F);
 
             using (CanvasPathBuilder cp = new CanvasPathBuilder(sender))
             {
@@ -93,94 +169,25 @@ namespace InfinityGroup.VesselMonitoring.Controls
 
                 using (var geometry = CanvasGeometry.CreatePath(cp))
                 {
+                    ds.Transform *= System.Numerics.Matrix3x2.CreateRotation(RadiansFromDegrees(this.Value)) * System.Numerics.Matrix3x2.CreateTranslation(this.Center);
                     ds.FillGeometry(geometry, Colors.Blue);
                 }
             }
 
-            float circle5Radius = (float)sender.ActualWidth / 12;
-            float circle4Radius = (float)sender.ActualWidth / 10;
-            float circle3Radius = (float)sender.ActualWidth / 7;
+            float circle5Radius = (float)sender.ActualWidth / 12F;
+            float circle4Radius = (float)sender.ActualWidth / 10F;
+            float circle3Radius = (float)sender.ActualWidth / 7F;
             float circle2Radius = (float)sender.ActualWidth / 6.5F;
             float circle1Radius = (float)sender.ActualWidth / 6.0F;
+            Vector2 upperLeftCorner = new Vector2(0, 0);
 
-            ds.FillCircle(this.Center, circle1Radius, Colors.DarkGray);
-            ds.FillCircle(this.Center, circle2Radius, Colors.Black);
-            ds.FillCircle(this.Center, circle3Radius, Colors.DarkGray);
-            ds.FillCircle(this.Center, circle4Radius, Colors.White);
-            ds.FillCircle(this.Center, circle5Radius, Colors.DarkGray);
+            ds.FillCircle(upperLeftCorner, circle1Radius, Colors.DarkGray);
+            ds.FillCircle(upperLeftCorner, circle2Radius, Colors.Black);
+            ds.FillCircle(upperLeftCorner, circle3Radius, Colors.DarkGray);
+            ds.FillCircle(upperLeftCorner, circle4Radius, Colors.White);
+            ds.FillCircle(upperLeftCorner, circle5Radius, Colors.DarkGray);
+            ds.DrawCircle(upperLeftCorner, radius, this.GaugeColor, 4F);
 
-            float radius = (float)sender.ActualWidth / 2F - 2F;
-            ds.DrawCircle(this.Center, radius, this.GaugeColor, 4F);
-
-            float minorTicLength = 15;
-            float minorTicstrokeWidth = 3;
-            for (int i = 0; i < 360; i += 10)
-            {
-                float x1 = (float)Math.Cos(RadiansFromDegrees(i)) * radius;
-                float y1 = (float)Math.Sin(RadiansFromDegrees(i)) * radius;
-                float x2 = (float)Math.Cos(RadiansFromDegrees(i)) * (radius - minorTicLength);
-                float y2 = (float)Math.Sin(RadiansFromDegrees(i)) * (radius - minorTicLength);
-                using (CanvasPathBuilder cp = new CanvasPathBuilder(sender))
-                {
-                    cp.BeginFigure(new Vector2(x1, y1) + this.Center);
-                    cp.AddLine(new Vector2(x2, y2) + this.Center);
-                    cp.EndFigure(CanvasFigureLoop.Open);
-                    using (var geometry = CanvasGeometry.CreatePath(cp))
-                    {
-                        ds.DrawGeometry(geometry, this.GaugeColor, minorTicstrokeWidth);
-                    }
-                }
-            }
-
-            float majorTicLength = 30;
-            float majorTicstrokeWidth = 6;
-            for (int i=0; i<360; i+=30)
-            {
-                float x1 = (float)Math.Cos(RadiansFromDegrees(i)) * radius;
-                float y1 = (float)Math.Sin(RadiansFromDegrees(i)) * radius;
-                float x2 = (float)Math.Cos(RadiansFromDegrees(i)) * (radius - majorTicLength);
-                float y2 = (float)Math.Sin(RadiansFromDegrees(i)) * (radius - majorTicLength);
-                using (CanvasPathBuilder cp = new CanvasPathBuilder(sender))
-                {
-                    cp.BeginFigure(new Vector2(x1,y1) + this.Center);
-                    cp.AddLine(new Vector2(x2, y2) + this.Center);
-                    cp.EndFigure(CanvasFigureLoop.Open);
-                    using (var geometry = CanvasGeometry.CreatePath(cp))
-                    {
-                        ds.DrawGeometry(geometry, this.GaugeColor, majorTicstrokeWidth);
-                    }
-                }
-
-                float atX = (float)Math.Cos(RadiansFromDegrees(i)) * (radius - 1.8F*majorTicLength);
-                float atY = (float)Math.Sin(RadiansFromDegrees(i)) * (radius - 1.8F*majorTicLength);
-                Vector2 at = new Vector2(atX, atY) + this.Center;
-
-                using (var textFormat = new CanvasTextFormat()
-                {
-                    HorizontalAlignment = CanvasHorizontalAlignment.Center,
-                    VerticalAlignment = CanvasVerticalAlignment.Center,
-                    FontSize = (float)this.LabelsFontSize * 1.5F,
-                })
-                {
-                    int label = (i+90) % 360;
-                    if (000 == label)      ds.DrawText("N", at, this.GaugeColor, textFormat);
-                    else if (090 == label) ds.DrawText("E", at, this.GaugeColor, textFormat);
-                    else if (180 == label) ds.DrawText("S", at, this.GaugeColor, textFormat);
-                    else if (270 == label) ds.DrawText("W", at, this.GaugeColor, textFormat);
-                    else               ds.DrawText(i.ToString(), at, this.GaugeColor, textFormat);
-                }
-            }
-
-            using (var textFormat = new CanvasTextFormat()
-            {
-                HorizontalAlignment = CanvasHorizontalAlignment.Right,
-                VerticalAlignment = CanvasVerticalAlignment.Bottom,
-                FontSize = (float)this.TextFontSize,
-            })
-            {
-                Vector2 at = new Vector2((float)sender.ActualWidth, (float)sender.ActualHeight);
-                ds.DrawText("Magnetic", at, this.GaugeColor, textFormat);
-            }
         }
 
         protected Vector2 Center { get; set; }
@@ -284,7 +291,7 @@ namespace InfinityGroup.VesselMonitoring.Controls
 
         static protected float RadiansFromDegrees(double degrees)
         {
-            return (float)(degrees * Math.PI / 180F);
+            return (float)((degrees % 360) * Math.PI / 180F);
         }
     }
 }
